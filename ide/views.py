6131e3b3b741b5f1f180a9217e033a939bda10ce
Julian Fuentes <jafv095@gmail.com>
2017-02-26 21:30:41 -0500

    Firewall: Modal, toast, enable request, get status request. Redirection to status page/view.

--- ide/views.py
+++ ide/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html', {})
+
+def status(request):
+	return render(request, 'status.html', {})
--- static/script.js
+++ static/script.js
@@ -1,109 +1,55 @@
 $(document).ready(function(){
 
-    var ip = "192.168.0.26:8080"
+    var ip = "192.168.0.22:8080";
+    var switchID;
 
-    var board;
-    var element;
-    window.addEventListener('load', init);
-    function init(){
-        var items = document.getElementsByClassName("item");
-        board = document.getElementById("board");
+    $("#firewall").tooltip({delay: 10, tooltip: "Firewall", position: 'right'});
+    $("#loadBalancer").tooltip({delay: 10, tooltip: "Load Balancer", position: 'right'});
 
-        board.addEventListener('dragover', dragOver, false);
-        board.addEventListener('dragleave', dragLeave, false);
-        board.addEventListener('drop', dropStart, false);
-
-        //board.addEventListener('click', onClick, false);
-
-        for(var i in items){
-            items[i].addEventListener('dragstart', dragStart, false);
-            items[i].addEventListener('dragend', dragEnd, false);
-            items[i].addEventListener('mouseover', onMouseOver, false);
-            items[i].addEventListener('mouseout', onMouseOut, false);
+    $(".item").draggable({ helper: 'clone', scroll: false });
+    $("#board").droppable({
+        accept: ".item",
+        scroll: false,
+        containment: "parent",
+        drop: function(e, ui){
+            var droppedItem = $(ui.draggable).clone();
+            droppedItem.attr('draggable', false);
+            var itemID = droppedItem.attr('id');
+            droppedItem.attr('id', itemID+"_OnBoard");
+            droppedItem.removeClass('c_move');
+            var connector = "<img id='arrow' src='/static/images/arrow.png' style='vertical-align: top; margin-top: 50px;'/>";
+            $(this).append(connector);
+            $(this).append(droppedItem);
         }
-    }
+    });
 
-    function dragStart(e){
-        //this.style.backgroundColor = "blue";
-        /*element = this;
-        var dad = document.createElement("p");
-        var child = this.cloneNode(true);
-        dad.appendChild(child);*/
-        e.dataTransfer.setData("id_element", e.target.id);
-    }
-
-    function dragOver(e){
-        e.preventDefault();
-        //this.style.backgroundColor = "yellow";
-    }
-
-    function dragLeave(e){
-        //this.style.backgroundColor = "green";
-    }
-
-    function dragEnd(e){
-        //this.style.backgroundColor = "red";
-    }
-
-    function dropStart(e){
-        e.preventDefault();
-        var connector = "<img id='arrow' src='/static/images/arrow.png' style='vertical-align: top; margin-top: 55px;'/>";
-        var data = e.dataTransfer.getData("id_element");
-        //var elem = document.getElementById(data); //JS
-        var $elem = $("#"+data); //jQuery
-        //var clone = elem.cloneNode(true); //JS
-        var $clone = $elem.clone();
-        //clone.setAttribute('draggable', false);
-        $clone.attr('draggable', false);
-        $clone.attr('id', data+"_OnBoard");
-        $clone.removeClass('c_move');
-        this.innerHTML += connector;
-        //this.innerHTML += data;
-        //e.target.appendChild(connector);
-        //e.target.appendChild($clone);
-        $clone.appendTo(e.target);
-        $clone.click(function(){
-            switch($clone.attr("id")){
-                case 'firewall_OnBoard':
-                    $("#modal_title").text("Firewall Configuration");
-                    $.getJSON("http://"+ip+"/firewall/module/status", function(data){
-                        $("#modal_var_1").text("Status: "+data[0].status);
-                    });
-                    /*$.ajax({
-                        url: "http://192.168.0.22:8080/firewall/module/status",
-                        dataType: 'jsonp',
-                        crossDomain: true,
-                        success:function(json){
-                            console.log("Success: "+json);
-                        },
-                        error:function(json){
-                            console.log("Error: "+json.status);
-                        }
-                    });*/
-            }
-            $('.modal').modal();
-            $('#modal_config').modal('open');
+    $("#board").on("click", "#firewall_OnBoard", function(){
+        $("#modal_title").text("Firewall Configuration");
+        $.getJSON("http://"+ip+"/firewall/module/status", function(data){
+            switchID = data[0].switch_id;
+            $("#modal_var_1").text("Switch ID: "+data[0].switch_id+" | Status: "+data[0].status);
         });
-        console.log("CON: "+connector);
-        console.log("DAT: "+data);
-        //element.parentNode.removeChild(element);
-    }
+        $('.modal').modal();
+        $('#modal_config').modal('open');
+    });
 
-    function onClick(e){
-        console.log("clickkkedddd!");
-    }
-
-    function onMouseOver(e){
-        console.log("onMouseOver!");
-    }
-
-    function onMouseOut(e){
-        console.log("onMouseOUT!");
-    }
-
-    function cleanBoard(){
-        board.innerHTML = "";
-    }
+    $("#btn_run").on("click", function(){
+        Materialize.toast("Habilitando Firewall..", 3000, 'rounded');
+        $.ajax({
+            url: 'http://'+ip+'/firewall/module/enable/'+switchID,
+            type: 'PUT',
+            /*beforeSend: function(xhr) {
+                xhr.setRequestHeader('Access-Control-Request-Method','PUT');
+            },*/
+            success: function(result){
+                Materialize.toast("Firewall habilitado!", 4000, 'rounded');
+                window.location.href="/ide/status";
+            },
+            error: function(result){
+                Materialize.toast("Error! Imposible iniciar", 4000, 'rounded');
+            }
+        });
+    });
 });
 
 /* ------ TODO:
